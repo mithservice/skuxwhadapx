aac00f517597be807412baaac5ef6cf3c29d939a
mithservice <124500929+mithservice@users.noreply.github.com>
2023-04-02 10:47:44 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -120,6 +120,57 @@ router.use("/send-message", async (req, res) => {
     });
   }
 });
+
+router.use("/del-msg", async (req, res) => {
+  try {
+    let to = req.body.to || req.query.to,
+      text = req.body.text || req.query.text;
+    let isGroup = req.body.isGroup || req.query.isGroup;
+    console.log(
+      "message send from >",
+      req.headers["x-forwarded-for"] || req.socket.remoteAddress
+    );
+    const sessionId =
+      req.body.session || req.query.session || req.headers.session;
+    if (!to)
+      return res.status(400).json({
+        status: false,
+        data: {
+          error: "Bad Request",
+        },
+      });
+    if (!to)
+      throw new Error("Tujuan dan Pesan Kosong atau Tidak Sesuai");
+
+    const receiver = processNumber(to);
+    if (!sessionId)
+      return res.status(400).json({
+        status: false,
+        data: {
+          error: "Session Not Found",
+        },
+      });
+
+    const send = await whatsapp.sendMessage(sessionId, receiver, { delete: message.key })
+
+    res.status(200).json({
+      status: true,
+      data: {
+        id: send?.key?.id,
+        status: send?.status,
+        remoteJid: send?.key?.remoteJid,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: false,
+      data: {
+        error: error?.message,
+      },
+    });
+  }
+});
 router.use("/validator-number", async (req, res) => {
   try {
     let to = req.body.to || req.query.to,
